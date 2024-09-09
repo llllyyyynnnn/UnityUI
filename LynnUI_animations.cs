ddbf283d5e2469e50a1a8df21a38a74d75068672
llllyyyynnnn <157394687+llllyyyynnnn@users.noreply.github.com>
2024-09-09 13:38:17 +0200

    added vec2 & vec3 support for animations

--- LynnUI_animations.cs
+++ LynnUI_animations.cs
@@ -8,4 +8,7 @@ public class LynnUI_Animations : ScriptableObject
 {
     public static float Smooth(float current, float goal, float speed) => current + (Time.deltaTime * (goal - current) * speed);
     public static Color Smooth(Color current, Color goal, float speed) => new Color(Smooth(current.r, goal.r, speed), Smooth(current.g, goal.g, speed), Smooth(current.b, goal.b, speed), Smooth(current.a, goal.a, speed));
+    public static Vector2 Smooth(Vector2 current, Vector2 goal, float speed) => new Vector2(Smooth(current.x, goal.x, speed), Smooth(current.y, goal.y, speed));
+    public static Vector3 Smooth(Vector3 current, Vector3 goal, float speed) => new Vector3(Smooth(current.x, goal.x, speed), Smooth(current.y, goal.y, speed), Smooth(current.z, goal.z, speed));
+    
 }
